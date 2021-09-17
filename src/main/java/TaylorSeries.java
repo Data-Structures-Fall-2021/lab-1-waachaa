f87ffafd3f7dfbea9c4a9f4df3413d857f521556
waachaa <58870161+waachaa@users.noreply.github.com>
2021-09-17 16:32:20 -0400

    Update TaylorSeries.java

--- src/main/java/TaylorSeries.java
+++ src/main/java/TaylorSeries.java
@@ -1,5 +1,19 @@
+package Lab01;
+
 public class TaylorSeries {
+    public static long pow(int a, int b) {
+        if (b != 0) {return (a*pow(a, b-1));} else return 1;
+    }
 
-    // YOUR CODE GOES HERE
+    public static long factorial(long n) {
+        if (n<=1) {return 1;} else return n*factorial(n-1);
+    }
 
+    public static double approximateExp(int x, int numIterations) {
+        double total = 0.0;
+        for (int i=0; i<numIterations; i++) {
+            total += (double)pow(x, i)/factorial(i);
+        }
+        return total;
+    }
 }
